555e2cad67be5b600d2aa7e2052665f49b06e428
MARCELO BAIRRO SARATE <marcelo.bsarate@senacsp.edu.br>
2025-02-06 09:22:27 -0300

    Primeiro Commit

--- Program.cs
+++ Program.cs
@@ -163,6 +163,8 @@ namespace primeiroSistema
             >= maior e igual
             <= menor e igual
             */
+            Console.WriteLine("Hello word!");
+            Console.WriteLine("Olá mundo");
 
         }
     }
